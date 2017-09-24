e7786154efb4cd173815fd4d2dd04b4225dc1f2d
shahzeb <zain@evolverstech.com>
2017-09-24 16:41:35 +0500

    adds fixes for map overlays

--- src/components/map-view/mapview.js
+++ src/components/map-view/mapview.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import FaAnchor from "react-icons/lib/fa/anchor";
 import { compose, withProps, withState, withHandlers } from "recompose";
 import {
   withScriptjs,
@@ -9,49 +10,64 @@ import {
 } from "react-google-maps";
 
 const MapWithControlledZoom = compose(
-	  withProps({
-	    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
-	    loadingElement: <div style={{ height: `100%` }} />,
-	    containerElement: <div style={{ height: `400px` }} />,
-	    mapElement: <div style={{ height: `50%` }} />,
-	  }),
-	  withState('zoom', 'onZoomChange', 8),
-	  withHandlers(() => {
-	    const refs = {
-	      map: undefined,
-	    }
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `50%` }} />,
+  }),
+  withState('zoom', 'onZoomChange', 8),
+  withHandlers(() => {
+    const refs = {
+      map: undefined,
+    }
 
-	    return {
-	      onMapMounted: () => ref => {
-	        refs.map = ref
-	      },
-	      onZoomChanged: ({ onZoomChange }) => () => {
-	        onZoomChange(refs.map.getZoom())
-	      }
-	    }
-	  }),
-	  withScriptjs,
-	  withGoogleMap
-	)(props =>
-	  <GoogleMap
-	    defaultCenter={{ lat: -34.397, lng: 150.644 }}
-	    zoom={props.zoom}
-	    ref={props.onMapMounted}
-	    onZoomChanged={props.onZoomChanged}
-	    onClick={props.rootClick}
-	  >
-	    <Marker
-	      position={{ lat: -34.397, lng: 150.644 }}
-	      onClick={props.onToggleOpen}>
-	    </Marker>
-	  </GoogleMap>
-	);
+    return {
+      onMapMounted: () => ref => {
+        refs.map = ref
+      },
+      onZoomChanged: ({ onZoomChange }) => () => {
+        onZoomChange(refs.map.getZoom())
+      }
+    }
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    zoom={props.zoom}
+    ref={props.onMapMounted}
+    onZoomChanged={props.onZoomChanged}
+  >
+    <Marker
+      position={{ lat: -34.397, lng: 150.644 }}
+      onClick={props.onToggleOpen}
+    >
+      <InfoWindow onCloseClick={props.onToggleOpen}>
+        <div>
+          <FaAnchor />
+          {" "}
+          Controlled zoom: {props.zoom}
+        </div>
+      </InfoWindow>
+    </Marker>
+  </GoogleMap>
+);
 
 class MapView extends Component {
 
+	constructor(){
+		super();
+		this.state = {
+			lat: 0,
+			lng: 0
+		}
+	}
+
 	render(){
 		return (
-			<MapWithControlledZoom rootClick={this.props.mapClicked}/>
+			<MapWithControlledZoom location={this.props.lat,this.props.lng} rootClick={this.props.mapClicked}/>
 		)
 	}
 
